ec79a13ae4c69cf721e08bb9c665c64a09834860
Kauê Fraga Rodrigues <rkauefraga@gmail.com>
2024-05-27 21:09:52 -0300

    config: init nextjs project with app router, typescript, eslint and tailwindcss

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Octopus',
+  description: 'Um amigo do trabalhador independente. Você é um freelancer? Com oito tentáculos, o Octopus pode te ajudar!',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='pt-br'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className='flex flex-col justify-center h-screen items-center gap-2'>
+      <h1 className="text-5xl font-bold">Octopus</h1>
+      <p className="text-xl">sendo construído em <a className="font-mono underline decoration-lime-600" href="https://github.com/kauefraga/octopus">kauefraga/octopus</a>...</p>
+    </main>
+  );
+}

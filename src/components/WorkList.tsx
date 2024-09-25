722261733a227ec5285603a2d8171d590a9228fa
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-09-24 21:38:46 -0300

    feat: add styles and work status

--- src/components/WorkList.tsx
+++ src/components/WorkList.tsx
@@ -2,6 +2,17 @@
 
 import Image from 'next/image';
 import { useWorkStore } from '@/stores/WorkStore';
+import { Work } from '@/schemas/Work';
+
+function WorkStatus({ status }: Pick<Work, 'status'>) {
+  const colors: Record<typeof status, string> = {
+    todo: 'bg-black/40',
+    doing: 'bg-yellow-500',
+    done: 'bg-green-500',
+  };
+
+  return <div className={`h-5 w-5 rounded-full ${colors[status]}`}></div>;
+}
 
 export default function WorkList() {
   const { works } = useWorkStore();
@@ -22,15 +33,19 @@ export default function WorkList() {
   }
 
   return (
-    <div className="mt-10 flex w-full flex-col items-center space-y-5">
-      <header className="flex w-full justify-start text-xl">
-        <h2>Lista de trabalhos</h2>
+    <div className="mt-10 flex w-full flex-col items-center gap-8">
+      <header className="w-full">
+        <h2 className="text-xl">Lista de trabalhos</h2>
       </header>
 
-      <ul className="space-y-2">
+      <ul className="w-full space-y-2">
         {works.map((work) => (
-          <li key={work.id}>
-            <p>{work.name}</p>
+          <li
+            key={work.id}
+            className="flex items-center justify-between rounded border border-black/80 px-6 py-4 shadow-lg"
+          >
+            <p className="w-2/3 truncate">{work.name}</p>
+            <WorkStatus status={work.status} />
           </li>
         ))}
       </ul>
